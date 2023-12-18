a0e3eab3e58dcde9ae733b8d5895535c1245558c
Restyled.io <commits@restyled.io>
2023-12-18 18:22:28 +0000

    Restyled by prettier

--- models/user.js
+++ models/user.js
@@ -1,23 +1,23 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-  username : {type : String, required : true, unique : true, trim : true},
-  email : {type : String, required : true, unique : true, trim : true},
-  password : {type : String, required : true}
+  username: { type: String, required: true, unique: true, trim: true },
+  email: { type: String, required: true, unique: true, trim: true },
+  password: { type: String, required: true },
 });
 
 // Password hashing middleware
-userSchema.pre('save', async function(next) {
-  if (this.isModified('password')) {
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 8);
   }
   next();
 });
 
 // Method to check password
-userSchema.methods.comparePassword = function(candidatePassword) {
+userSchema.methods.comparePassword = function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,63 +1,62 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Task =
-    require('../models/task'); // Adjust the path as per your project structure
+const Task = require("../models/task"); // Adjust the path as per your project structure
 
 // GET all tasks
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const tasks = await Task.find();
     res.json(tasks);
   } catch (error) {
-    res.status(500).json({message : error.message});
+    res.status(500).json({ message: error.message });
   }
 });
 
 // GET a single task by ID
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const task = await Task.findById(req.params.id);
-    if (!task)
-      return res.status(404).json({message : 'Task not found'});
+    if (!task) return res.status(404).json({ message: "Task not found" });
     res.json(task);
   } catch (error) {
-    res.status(500).json({message : error.message});
+    res.status(500).json({ message: error.message });
   }
 });
 
 // POST a new task
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const task = new Task(req.body);
   try {
     const newTask = await task.save();
     res.status(201).json(newTask);
   } catch (error) {
-    res.status(400).json({message : error.message});
+    res.status(400).json({ message: error.message });
   }
 });
 
 // PUT to update a task
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
-    const updatedTask =
-        await Task.findByIdAndUpdate(req.params.id, req.body, {new : true});
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
     if (!updatedTask)
-      return res.status(404).json({message : 'Task not found'});
+      return res.status(404).json({ message: "Task not found" });
     res.json(updatedTask);
   } catch (error) {
-    res.status(400).json({message : error.message});
+    res.status(400).json({ message: error.message });
   }
 });
 
 // DELETE a task
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const taskToDelete = await Task.findByIdAndDelete(req.params.id);
     if (!taskToDelete)
-      return res.status(404).json({message : 'Task not found'});
-    res.json({message : 'Task deleted successfully'});
+      return res.status(404).json({ message: "Task not found" });
+    res.json({ message: "Task deleted successfully" });
   } catch (error) {
-    res.status(500).json({message : error.message});
+    res.status(500).json({ message: error.message });
   }
 });
 
